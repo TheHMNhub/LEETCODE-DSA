80b83519e73b2c40f34b3bf01254c5d698791407
Hrigved Naidu <163576509+TheHMNhub@users.noreply.github.com>
2026-07-11 23:38:35 +0530

    [1608] [Time Beats: 100.00%] [Memory Beats: 89.02%] - LeetPush

--- 1608-Special-Array-With-X-Elements-Greater-Than-or-Equal-X.cpp
+++ 1608-Special-Array-With-X-Elements-Greater-Than-or-Equal-X.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
     int specialArray(vector<int>& nums) {
-        int count = 0, x, ans = -1;;
-        //Brute Force Approach
+    //     int count = 0, x, ans = -1;;
+    //     //Brute Force Approach
 
-        for(int i = 0; i< nums.size(); i++)
+    //     for(int i = 0; i< nums.size(); i++)
+    //     {
+    //         x = i + 1;
+    //         count = 0;
+    //         for(int j = 0; j< nums.size(); j++)
+    //         {
+    //             if(x<= nums[j])
+    //                 count++;
+    //         }
+
+    //         if(count==x)
+    //         ans = x;
+    //     }
+    //     return ans;
+    // }
+
+    // Binary Search Approach
+    int s = 0, e = nums.size(), mid, count = 0;
+    while(s<=e)
+    {
+        mid = s + (e-s)/2;
+        count = 0;
+        for(int i = 0; i<nums.size(); i++)
         {
-            x = i + 1;
-            count = 0;
-            for(int j = 0; j< nums.size(); j++)
-            {
-                if(x<= nums[j])
-                    count++;
-            }
-
-            if(count==x)
-            ans = x;
+            if(mid <= nums[i])
+            count++;
         }
-        return ans;
+        //If count of element x(i.e mid in this case) is exactly x then , it's our answer
+        if(count==mid)
+        return mid;
+        //if count is greater then x(i.e mid) then increase value of x(mid)
+        else if(count>mid)
+        s = mid + 1;
+        //else decrease the value of x(mid)
+        else
+        e = mid - 1;
     }
-
-    
+    //if we dont find any such x the return -1
+    return -1;
+    }
 };
